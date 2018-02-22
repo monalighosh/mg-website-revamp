108b8dcb7200cc63d33718cfed5ac6c5e2004468
Monali Ghosh <monalighosh@users.noreply.github.com>
2018-02-23 01:10:14 +1100

    Setting up gulp

---
+++ website/gulpfile.js
@@ -0,0 +1,59 @@
+"use strict";
+// Variables
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const htmlmin = require("gulp-htmlmin");
+const cleanCSS = require("gulp-clean-css");
+const uglify = require("gulp-uglify");
+const babel = require("gulp-babel");
+const concat = require("gulp-concat");
+const imagemin = require("gulp-imagemin");
+
+// Files paths
+const htmlFiles = "src/**/*.html";
+const sassFiles = "src/sass/**/*.scss";
+const imageFiles = "src/images/**/*";
+const jsFiles = "src/js/**/*.js";
+
+// HTML Tasks
+gulp.task("html", function(){
+  return gulp.src(htmlFiles)
+  .pipe(htmlmin({collapseWhitespace: true}))
+  .pipe(gulp.dest("build"));
+});
+
+// CSS Tasks
+gulp.task("css", function(){
+  return gulp.src(sassFiles)
+  .pipe(sass().on("error", sass.logError))
+  .pipe(cleanCSS())
+  .pipe(gulp.dest("build/css"))
+});
+
+// Images Tasks
+gulp.task("images", function(){
+  return gulp.src(imageFiles)
+  .pipe(imagemin())
+  .pipe(gulp.dest("build/images"));
+});
+
+// JS Tasks
+gulp.task("js", function(){
+  return gulp.src(jsFiles)
+  .pipe(babel({presets: ["env"]}))
+  .pipe(concat("all.js"))
+  .pipe(uglify())
+  .pipe(gulp.dest("build/js"));
+});
+
+// Watch Tasks
+gulp.task("watch", function(){
+  gulp.watch(htmlFiles, ["html"]);
+  gulp.watch(sassFiles, ["css"]);
+  gulp.watch(imageFiles, ["images"]);
+  gulp.watch(jsFiles, ["js"]);
+});
+
+// Default Task
+gulp.task("default", ["html", "css", "images", "js", "watch"]);
